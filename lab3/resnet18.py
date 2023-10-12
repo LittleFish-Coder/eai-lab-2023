6c99e791c86cb0e4e070bc75f0abacc0fe5e3092
LittleFish-Coder <adrianyu890317@gmail.com>
2023-10-12 19:15:04 +0800

    finish lab3 model

--- lab3/resnet18.py
+++ lab3/resnet18.py
@@ -9,28 +9,17 @@ class ResBlock(nn.Module):
         super(ResBlock, self).__init__()
         # defines two consecutive convolutional layers within the residual block
         self.left = nn.Sequential(
-            nn.Conv2d(
-                inchannel,
-                outchannel,
-                kernel_size=3,
-                stride=stride,
-                padding=1,
-                bias=False,
-            ),
+            nn.Conv2d(inchannel, outchannel, kernel_size=3, stride=stride, padding=1, bias=False),
             nn.BatchNorm2d(outchannel),
             nn.ReLU(inplace=True),
-            nn.Conv2d(
-                outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False
-            ),
+            nn.Conv2d(outchannel, outchannel, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(outchannel),
         )
         self.shortcut = nn.Sequential()
         if stride != 1 or inchannel != outchannel:
             # shortcut, in order to maintain consistency with the structure of the two convolutional layers' results, some processing is required here.
             self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    inchannel, outchannel, kernel_size=1, stride=stride, bias=False
-                ),
+                nn.Conv2d(inchannel, outchannel, kernel_size=1, stride=stride, bias=False),
                 nn.BatchNorm2d(outchannel),
             )
 
@@ -48,7 +37,8 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.inchannel = 64
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False),  # for CIFAR10, use 3x3 kernel with stride 1, incase of information loss
+            # nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=1, bias=False),   # original paper uses 7x7 kernel with stride 2
             nn.BatchNorm2d(64),
             nn.ReLU(),
         )
